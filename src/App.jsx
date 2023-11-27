33e5a0544a361c09da172b4c1e69278b75fc1836
CodingShields <139907470+CodingShields@users.noreply.github.com>
2023-11-26 17:31:03 -0700

    stats modal

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,13 @@ import pokeBackground from "./assets/pokeBackground.jpg";
 function App() {
 	const [pokeData, setPokeData] = useState([]);
 	const [userSearch, setUserSearch] = useState("");
-	const [multipleNames, setMultipleNames] = useState([]);
+	const [multipleNames, setMultipleNames] = useState([
+		{
+			id: 0,
+			name: "Please Select A Pokemon",
+			url: "",
+		},
+	]);
 	const [userSearchResponse, setUserSearchResponse] = useState([]);
 	const [state, setState] = useState({
 		loading: false,
@@ -12,6 +18,8 @@ function App() {
 		errorMessage: "",
 		searchedDataSingleMatch: false,
 		searchedDataMultipleMatch: false,
+		statsModal: false,
+		clearStats: false,
 	});
 
 	useEffect(() => {
@@ -39,13 +47,15 @@ function App() {
 	const handleSearch = () => {
 		const characterName = userSearch;
 		const formatText = characterName.charAt(0).toLowerCase() + characterName.slice(1);
-		setUserSearch(formatText)
-		const result = pokeData.filter((item) => item.name.startsWith(formatText));
-		if (result.length === 0) {
-		} else if (result.length > 0) {
-				result.forEach((pokemon) => {
-				setMultipleNames((prevState) => [
-					...prevState,
+		setUserSearch(formatText);
+		const searchData = pokeData.filter((item) => item.name === formatText);
+		console.log(searchData, "searchData");
+		if (searchData.length === 0) {
+			const wideDataSearch = pokeData.filter((item) => item.name.startsWith(formatText));
+			console.log(wideDataSearch, "wideDataSearch");
+			wideDataSearch.forEach((pokemon) => {
+				setMultipleNames((prevNames) => [
+					...prevNames,
 					{
 						id: nanoid(),
 						name: pokemon.name,
@@ -53,9 +63,11 @@ function App() {
 					},
 				]);
 			});
+			console.log(multipleNames, "multipleNames");
 			setState({ searchedDataMultipleMatch: true });
 		} else {
-			const characterUrl = result.url;
+			const characterUrl = searchData[0].url;
+			console.log(characterUrl, "characterUrl");
 			const displayText = characterName.charAt(0).toUpperCase() + characterName.slice(1);
 			const fetchSprite = async () => {
 				try {
@@ -64,20 +76,22 @@ function App() {
 						throw new Error("Something went wrong");
 					} else if (response.ok) {
 						const characterData = await response.json();
-							setMultipleNames((prevState) => [
+						console.log(characterData, "characterData");
+						setUserSearchResponse((prevState) => [
 							...prevState,
 							{
 								id: nanoid(),
-								name: pokemon.name,
-								url: pokemon.url,
+								name: displayText,
+								img: characterData.sprites.front_default,
+								weight: characterData.weight,
+								height: characterData.height,
 							},
-							]);
+						]);
 						setState({ searchedDataSingleMatch: true });
 						setUserSearch("");
-						console.log(userSearchResponse, "userSearchResponse");
 					}
 				} catch (error) {
-					console.log(error);
+					console.log(error, "error");
 				}
 			};
 			fetchSprite();
@@ -89,17 +103,26 @@ function App() {
 		setUserSearchResponse(newPokeCharacterList);
 	};
 
-
 	const handleCharacterSelect = (e) => {
 		const characterId = e.target.value;
 		const findCharacter = multipleNames.find((item) => item.id === characterId);
 		const characterName = findCharacter.name;
 		const characterUrl = findCharacter.url;
+		setUserSearch(characterName);
 		setState({ searchedDataMultipleMatch: false });
-	}
+	};
+
+	const handleStats = (id) => {
+		console.log(id, "clicked item");
+		setState({ statsModal: true });
+	};
+
+	const handleClearStats = () => {
+		setState({ clearStats: true });
+	};
 
 	return (
-		<div className='main'>
+		<div>
 			<div
 				style={{
 					display: "flex",
@@ -110,6 +133,20 @@ function App() {
 				}}
 				className='pokemon-container'
 			>
+				<div
+					style={{
+						display: state.statsModal ? "flex" : "none",
+					}}
+					className='modal'
+				>
+					<div className='modal-content'>
+						<h1>Stats</h1>
+
+						<button className='close-button' onClick={() => setState({ statsModal: false })}>
+							x
+						</button>
+					</div>
+				</div>
 				<img className='background' src={pokeBackground} />
 				<div className='poke-container-main'>
 					<input
@@ -117,30 +154,39 @@ function App() {
 						onChange={(e) => setUserSearch(e.target.value)}
 						className='poke-search-input'
 					></input>
-					<button onClick={handleSearch}>Search Pokemon</button>
-
+					<button className='poke-search-button' onClick={handleSearch}>
+						Search Pokemon
+					</button>
+					<button className='poke-search-button' onClick={handleClearStats}>
+						Clear Pokemon
+					</button>
 					{state.searchedDataSingleMatch
 						? userSearchResponse.map((item) => {
 								return (
 									<div key={item.id} className='user-search-poke-container'>
-										<h3 className='poke-character-name-text'>{item.name}</h3>
-										<button>Stats Here</button>
-										<img className='poke-character-img' src={item.img} />
-										<button onClick={() => handlePokeCharacterDelete(item.id)}>X</button>
+										<div className='poke-character-name-container'>
+											<h3 className='poke-character-name-text'>{item.name}</h3>
+											<p onClick={() => handleStats(item.id)} className='poke-stats-button'>
+												Stats Here
+											</p>
+										</div>
+										<div className='poke-character-img-container'>
+											<img className='poke-character-img' src={item.img} />
+										</div>
+										<button className='poke-delete-button' onClick={() => handlePokeCharacterDelete(item.id)}>
+											x
+										</button>
 									</div>
 								);
 						  })
 						: ""}
-
 					<div
 						style={{
 							display: state.searchedDataMultipleMatch ? "flex" : "none",
 						}}
 						className='multiple-names-container'
 					>
-						<select
-						onChange = {handleCharacterSelect}
-						>
+						<select className='pokemon-multiple-names-select' onChange={handleCharacterSelect}>
 							{multipleNames.map((item) => {
 								return (
 									<option key={item.id} value={item.id}>
